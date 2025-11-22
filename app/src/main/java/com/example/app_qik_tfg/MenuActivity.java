6ca0b3c5c3164da5746b22e12c30bfc7128a6f1f
Andres Luna <miosesfreddy123@gmail.com>
2025-11-22 18:54:04 +0100

    Feature: navegación a menú y carga de categorías - Implementada la navegación a MenuActivity mediante Intent. - MenuActivity muestra correctamente las categorías en el ListView. - Código reorganizado y limpio para mantener estructura de Activities.

---
+++ app/src/main/java/com/example/app_qik_tfg/MenuActivity.java
@@ -0,0 +1,44 @@
+package com.example.app_qik_tfg;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+
+    // Declaras un ListView donde mostrarás las categorías
+    ListView lvCategories;
+
+    // Arreglo con las categorías que se mostrarán en el ListView
+    String[] categories = {"Bebidassss", "Comidas", "Postres"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_menu);
+
+
+        // Conectas el ListView del layout con la variable en la clase
+        lvCategories = findViewById(R.id.lvCategories);
+
+        // Creas un adaptador que convierte el arreglo en elementos visuales de lista
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, categories);
+
+        // Asignas el adaptador al ListView
+        lvCategories.setAdapter(adapter);
+
+        // Evento al hacer clic en un ítem de la lista
+        lvCategories.setOnItemClickListener((parent, view, position, id) -> {
+
+            // Obtienes el nombre de la categoría seleccionada según su posición
+            String category = categories[position];
+
+        });
+    }
+}
